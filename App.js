36b3b12eea75d410ce20079a5ee7ca428863e6dd
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-04-11 07:08:19 +0100

    Redux set up and implemented

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return(
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Lets build!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   )
 }
 
@@ -19,4 +23,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App
+export default App;

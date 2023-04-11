84b111e37e49a720730245f81bc3dcfebdf3fba6
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-04-11 18:21:48 +0100

    Implemented google autocomplete library into homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,50 @@
-import { StyleSheet, View, Image } from 'react-native'
+import { View, Image } from 'react-native'
 import React from 'react'
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { setOrigin, setDestination } from '../slices/navSlice'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+
   return (
     <View style={tw`bg-red-300`}>
       <Image 
         style={[tw`w-35 h-35 ml-2`, {resizeMode: "contain"}]}
         source={require("../assets/uber_logo.png")}
+      />
+      <GooglePlacesAutocomplete
+        placeholder='Where from?'
+        nearbyPlacesAPI='GooglePlacesSearch'
+        debounce={400}
+        styles={{
+          container: {
+            flex: 0,
+          },
+          textInput: {
+            fontSize: 30
+          }
+        }}
+        enablePoweredByContainer={false}
+
+        onPress={(data, details = null) => {
+          dispatch(setOrigin({
+            location: details.geometry.location,
+            description: data.description
+          }))
+
+          dispatch(setDestination(null))
+        }}
+
+        fetchDetails={true}
+        returnKeyType={"search"}
+        query={{
+          key: GOOGLE_MAPS_APIKEY,
+          language: 'en',
+        }}
       />
       <NavOptions />
     </View>
